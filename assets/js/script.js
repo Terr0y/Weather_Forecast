330363c3a6db614b30f950ba1ce9c77a133093ab
Terry <olaterry208@yahoo.com>
2024-01-30 11:51:53 +0000

    added 5 days forecast on javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,11 @@ $(document).ready(function () {
     }
   });
 
+  // Here we are building the URL we need to query the database
   function getWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`;
+
+    // We then created an Fetch call
     fetch(url)
       .then(function (response) {
         console.log(response);
@@ -24,8 +27,8 @@ $(document).ready(function () {
 
         const currentWeather = {
           city: data.name,
-          date: new Date(),
-          icon: "Rainy",
+          date: new Date(data.dt * 1000).toDateString(),
+          icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
           temp: `${data.main.temp} °C`,
           wind: `${data.wind.speed} KPH`,
           humidity: `${data.main.humidity}%`,
@@ -34,7 +37,7 @@ $(document).ready(function () {
         // Populate the weather-section with current weather
         const weatherHtml = `
         <h2>${currentWeather.city} (${currentWeather.date})</h2>
-        <div>Icon: ${currentWeather.icon}</div>
+        <div><img src="${currentWeather.icon}" alt="${data.weather[0].description}"></div>
         <div>Temperature: ${currentWeather.temp}</div>
         <div>Wind Speed: ${currentWeather.wind}</div>
         <div>Humidity: ${currentWeather.humidity}</div>
@@ -48,26 +51,29 @@ $(document).ready(function () {
     fetch(forecastUrl)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         let forecastHtml = "<h3>5-Day Forecast:</h3>";
-        for (let i = 0; i < data.list.length; i += 8) {
-          const day = data.list[i];
-          forecastHtml += `
-              <div class="col-md-2 forecast-card">
-                <div class="card text-center">
-                  <div class="card-body">
-                    <h5 class="card-title">${new Date(
-                      day.dt_txt
-                    ).toDateString()}</h5>
-                    <img src="http://openweathermap.org/img/wn/${
-                      day.weather[0].icon
-                    }.png" alt="Weather icon">
-                    <p class="card-text">Temp: ${day.main.temp}°C</p>
-                    <p class="card-text">Wind: ${day.wind.speed} KPH</p>
-                    <p class="card-text">Humidity: ${day.main.humidity}%</p>
+        for (let i = 0; i < data.list.length; i++) {
+          if (data.list[i].dt_txt.includes("09:00:00")) {
+            const day = data.list[i];
+            forecastHtml += `
+                <div class="col forecast-card">
+                  <div class="card text-center card-height">
+                    <div class="card-body">
+                      <h5 class="card-title">${new Date(
+                        day.dt_txt
+                      ).toDateString()}</h5>
+                      <img src="http://openweathermap.org/img/wn/${
+                        day.weather[0].icon
+                      }.png" alt="Weather icon">
+                      <p class="card-text">Temp: ${day.main.temp}°C</p>
+                      <p class="card-text">Wind: ${day.wind.speed} KPH</p>
+                      <p class="card-text">Humidity: ${day.main.humidity}%</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            `;
+              `;
+          }
         }
         $("#forecast").html(forecastHtml);
       });
